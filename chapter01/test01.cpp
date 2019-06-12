07f775776d6c3ea54d52c2e7ea3d95d6ba77e0f1
yoyboy <534411590@qq.com>
2019-06-13 00:13:04 +0800

    cin, cout, cerr, clog

---
+++ chapter01/test01.cpp
@@ -0,0 +1,19 @@
+// 标准输入输出
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Enter two numbers: " << std::endl;
+    int x = 0, y = 0;
+
+    std::cin >> x >> y;
+
+    std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
+    
+    std::cerr << "error" << std::endl; // error
+
+    std::clog << "log" << std::endl; // log
+
+    return 0;
+}
