8f0483ccd3f7d4f2d013a3c225a6627e5100f43a
yoyboy <534411590@qq.com>
2019-07-18 00:00:00 +0800

    Update work6_2_3.cpp

--- chapter06/work6_2_3.cpp
+++ chapter06/work6_2_3.cpp
@@ -27,13 +27,6 @@ const string ex6_17_2(const string& str)
     return s;
 }
 
-// ex6_15
-/*
-    1. s是常量引用是因为读s对象的值, occurs是普通引用是因为需要不断写occurs的值
-    2. c不用引用类型是因为只需要用到c的值, 并且本身占用空间不大
-    3. 如果s是普通引用, 则不能用字面值初始化s, 必须用string对象
-    4. 如果occurs是常量引用, 则对occurs递增时会报错
- */
 
 int main(int argc, char const *argv[])
 {
