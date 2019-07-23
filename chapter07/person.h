a0185e603c74f49ef4943ed274ae1bc1fa7ef66f
yoyboy <534411590@qq.com>
2019-07-23 09:25:09 +0800

    work7.2 - about use class key_word

--- chapter07/person.h
+++ chapter07/person.h
@@ -3,30 +3,55 @@
 
 using namespace std;
 
-// ex7_4 ex7_5
-struct Person
+// // ex7_4 ex7_5
+// struct Person
+// {
+//     string name;
+//     string address;
+//     string get_name() const
+//     {
+//         return name;
+//     }
+//     string get_address() const
+//     {
+//         return address;
+//     }
+// };
+
+// // ex7_9
+// istream& read(istream& is, Person& person)
+// {
+//     is >> person.name >> person.address;
+//     return is;
+// }
+// ostream& print(ostream& os, const Person& person)
+// {
+//     os << "name: " << person.name << endl;
+//     os << "address: " << person.address;
+//     return os;
+// }
+
+// ex7_19
+class Person
 {
-    string name;
-    string address;
-    string get_name() const
-    {
-        return name;
-    }
-    string get_address() const
-    {
-        return address;
-    }
+    private:
+        string name;
+        string address;
+    public:
+        string get_name() const
+        {
+            return name;
+        }
+        string get_address() const
+        {
+            return address;
+        }
+    Person() = default;
+    Person(string name, string address): name(name), address(address){}
+    Person(istream&);
 };
 
-// ex7_9
-istream& read(istream& is, Person& person)
-{
-    is >> person.name >> person.address;
-    return is;
-}
-ostream& print(ostream& os, const Person& person)
-{
-    os << "name: " << person.name << endl;
-    os << "address: " << person.address;
-    return os;
-}
+// Person::Person(istream& is)
+// {
+//     read(is, *this);
+// }
