bdfbfd1bd0d3bafc6e78e9f10e2bf2208db6ea94
yoy <36720031+yoyboy@users.noreply.github.com>
2019-07-27 21:14:46 +0800

    work7.3.1 - about class Screen

---
+++ chapter07/work7_3_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// ex7_23, ex7_24, ex7_25
+class Screen
+{
+    public:
+        Screen() = default;
+        Screen(int width, int height): width(width), height(height){}
+        Screen(int width, int height, string content): width(width), height(height), content(content){}
+        void screen_print(void)
+        {
+            cout << width << " " << height << " " << content;
+        }
+    private:
+        int width = 0;
+        int height = 0;
+        string content = "";
+};
+
+int main(int argc, char const *argv[])
+{
+    Screen x;
+    Screen y(1, 10);
+    Screen z(10, 100, "vscode");
+    x.screen_print();
+    return 0;
+}
