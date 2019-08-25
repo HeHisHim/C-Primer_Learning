fd0275046a203435770142fbda1ba93eae7c602d
yoyboy <534411590@qq.com>
2019-08-25 12:21:19 +0800

    work9.2.7 - about compare vector

---
+++ chapter09/work9_2_7.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <deque>
+
+using namespace std;
+
+bool ex9_15(vector<int>& x, vector<int>& y)
+{
+    if(x.size() != y.size())
+    {
+        return false;
+    }
+    vector<int>::const_iterator xbegin = x.cbegin();
+    vector<int>::const_iterator ybegin = y.cbegin();
+    vector<int>::const_iterator xend = x.cend();
+    vector<int>::const_iterator yend = y.cend();
+    for(; xbegin != xend | ybegin != yend; xbegin++, ybegin++)
+    {
+        if(*xbegin != *ybegin)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char const* argv[])
+{
+    vector<int> x = {1, 2, 3, 4};
+    vector<int> y = {0, 9};
+    cout << ex9_15(x, y) << endl;
+    return 0;
+}
