b623e472655516fadecc92a41cf76c152572179b
yoyboy <534411590@qq.com>
2019-08-25 12:34:47 +0800

    work9.2.7 - about compare list and vector

--- chapter09/work9_2_7.cpp
+++ chapter09/work9_2_7.cpp
@@ -26,10 +26,33 @@ bool ex9_15(vector<int>& x, vector<int>& y)
     return true;
 }
 
+bool ex9_16(list<int>& x, vector<int>& y)
+{
+    if(x.size() != y.size())
+    {
+        return false;
+    }
+    list<int>::const_iterator xbegin = x.cbegin();
+    vector<int>::const_iterator ybegin = y.cbegin();
+    list<int>::const_iterator xend = x.cend();
+    vector<int>::const_iterator yend = y.cend();
+    for(; xbegin != xend | ybegin != yend; xbegin++, ybegin++)
+    {
+        if(*xbegin != *ybegin)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 int main(int argc, char const* argv[])
 {
     vector<int> x = {1, 2, 3, 4};
-    vector<int> y = {0, 9};
-    cout << ex9_15(x, y) << endl;
+    // vector<int> y = {0, 9};
+    // cout << ex9_15(x, y) << endl;
+    list<int> y = {1, 2, 3, 4};
+    cout << ex9_16(y, x) << endl;
     return 0;
 }
