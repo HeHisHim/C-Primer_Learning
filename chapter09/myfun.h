479eea78afa1dbe85c58bb17c931d8a14a10e1f4
yoyboy <534411590@qq.com>
2019-08-30 00:10:28 +0800

    Update myfun.h

--- chapter09/myfun.h
+++ chapter09/myfun.h
@@ -1,6 +1,8 @@
 #include <deque>
 #include <vector>
 #include <list>
+#include <string>
+#include <forward_list>
 #include <iostream>
 
 void print_vector(std::vector<int>& numbers)
@@ -22,6 +24,15 @@ void print_deque(std::deque<int>& numbers)
 }
 
 void print_list(std::list<int>& numbers)
+{
+    for(auto x : numbers)
+    {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+}
+
+void print_forward_list(std::forward_list<std::string>& numbers)
 {
     for(auto x : numbers)
     {
