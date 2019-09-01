f703e2c398e619a455d7ec019c0ad48faf09ab12
yoyboy <534411590@qq.com>
2019-09-01 23:39:43 +0800

    work9.4 - about element allocation

---
+++ chapter09/work9_4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// ex9_35
+/*
+    vector的capacity表示当前vector对象最大能容纳多少元素
+    vector的size表示当前vector当前的元素数量
+ */
+
+// ex9_37
+/*
+    list的存储方式不是连续存储
+    array元素数量固定
+ */
