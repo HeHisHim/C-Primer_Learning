1b02fa61be2a29eeffc216967e61264f708f4e69
yoyboy <534411590@qq.com>
2019-09-11 20:36:39 +0800

    work10.3.4 - bind function

---
+++ chapter10/work10_3_4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <functional>
+#include "myfun.h"
+
+using namespace std;
+using namespace placeholders;
+
+bool check_size(string& word, unsigned sz)
+{
+    return word.size() > sz;
+}
+
+void ex10_22(vector<string>& words, unsigned sz)
+{
+    auto x = count_if(words.begin(), words.end(), bind(check_size, _1, sz));
+    cout << x << endl;
+}
+
+void ex10_24(string word)
+{
+    vector<int> vec = {1, 2, 3, 4, 5, 6};
+    // auto result = find_if(vec.begin(), vec.end(), bind(check_size, word, _2));
+    auto result = find_if(vec.begin(), vec.end(), [](int& x){return x > 3;});
+    // cout << *result << endl;
+    cout << *result << endl;
+}
+
+int main(int argc, char const* argv[])
+{
+    vector<string> words = {"the", "quick", "red", "fox", "jumps", "over", "the", "slow", "banana"};
+    // ex10_22(words, 4);
+    ex10_24("abcd");
+    return 0;
+}
