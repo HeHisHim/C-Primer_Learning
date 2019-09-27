4911f4c91b8289f0f72bcb2ec3d3735cdda3c9c6
yoyboy <534411590@qq.com>
2019-09-27 09:52:00 +0800

    work10.3.2 - map insert return type

--- chapter11/work11_3_2.cpp
+++ chapter11/work11_3_2.cpp
@@ -2,23 +2,30 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <set>
+#include "myfun.h"
+#include <utility>
 
 using namespace std;
 
-void ex11_3(void)
+void ex11_20(void)
 {
     vector<string> words = {
         "example", "Example", "apple", "banana", "orange", "tea",
         "bag", "potato", "Potato"
     };
     map<string, size_t> word_count;
-    for(auto word : words)
+    for(auto& word : words)
     {
         for(auto& w : word)
         {
             w = tolower(w);
         }
-        ++word_count[word];
+        auto res = word_count.insert({word, 1});
+        if(!res.second)
+        {
+            res.first->second++;
+        }
     }
 
     for(auto& wc : word_count)
@@ -27,8 +34,15 @@ void ex11_3(void)
     }
 }
 
+// ex11_22
+/*
+    参数类型: pair<string, vector<int>>
+    返回类型: pair<map<string, vector<int>>::iterator, bool>
+ */
+
+
 int main(int argc, char const* argv[])
 {
-    ex11_3();
+    ex11_20();
     return 0;
 }
