a997eebfa6c8916f783febaadfa7841e5cdff2ed
yoyboy <534411590@qq.com>
2019-10-23 21:52:40 +0800

    Create strblob.h

---
+++ chapter12/strblob.h
@@ -0,0 +1,60 @@
+#include <memory>
+#include <vector>
+#include <stdexcept>
+#include <string>
+
+class StrBlob
+{
+    public:
+        typedef std::vector<std::string>::size_type size_type;
+        StrBlob();
+        StrBlob(std::initializer_list<std::string> il);
+        size_type size() const
+        {
+            return data->size();
+        }
+        bool empty() const
+        {
+            return data->empty();
+        }
+        void push_back(const std::string& t)
+        {
+            data->push_back(t);
+        }
+        void pop_back();
+        std::string& front();
+        std::string& back();
+    private:
+        std::shared_ptr<std::vector<std::string>> data;
+        void check(size_type i, const std::string& msg) const;
+};
+
+void StrBlob::check(StrBlob::size_type i, const std::string& msg) const
+{
+    if(i >= StrBlob::data->size())
+    {
+        throw std::out_of_range(msg);
+    }
+}
+
+void StrBlob::pop_back()
+{
+    StrBlob::check(0, "pop back on empty StrBlob");
+    StrBlob::data->pop_back();
+}
+
+std::string& StrBlob::front()
+{
+    StrBlob::check(0, "front on empty StrBlob");
+    return StrBlob::data->front();
+}
+
+std::string& StrBlob::back()
+{
+    StrBlob::check(0, "back on empty StrBlob");
+    return StrBlob::data->back();
+}
+
+StrBlob::StrBlob(): data(std::make_shared<std::vector<std::string>>()){}
+StrBlob::StrBlob(std::initializer_list<std::string> il): data(std::make_shared<std::vector<std::string>>(il)){}
+
