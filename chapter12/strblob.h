3441cdeb95141d0e97b80e2c948ed455dcdc5c0d
HeHisHim <534411590@qq.com>
2019-10-31 10:06:30 +0800

    Update strblob.h

--- chapter12/strblob.h
+++ chapter12/strblob.h
@@ -73,3 +73,18 @@ class StrBlobPtr
         std::weak_ptr<std::vector<std::string>> wptr;
         std::size_t curr;
 };
+
+std::shared_ptr<std::vector<std::string>> 
+StrBlobPtr::check(std::size_t i, const std::string& msg) const
+{
+    auto ret = wptr.lock();
+    if(!ret)
+    {
+        throw std::runtime_error("unbound StrBlobPtr");
+    }
+    if(i > ret->size())
+    {
+        throw std::out_of_range(msg);
+    }
+    return ret;
+}
