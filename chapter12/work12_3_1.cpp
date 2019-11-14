26f07732d6dee36aab0401189d62e10c08f6e6d9
HeHisHim <534411590@qq.com>
2019-11-14 09:21:16 +0800

    Update work12_3_1.cpp

--- chapter12/work12_3_1.cpp
+++ chapter12/work12_3_1.cpp
@@ -8,25 +8,24 @@
 
 using namespace std;
 
-// void runQueries(ifstream& infile)
-// {
-//     TextQuery tq(infile);
-//     while(true)
-//     {
-//         cout << "enter word to look for, or q to quit";
-//         string s;
-//         if(!(cin >> s) || "q" == s)
-//         {
-//             break;
-//         }
-//         print(cout, tq.query(s)) << endl;
-//     }
-// }
-
-int main(int argc, char const* argv[])
+void ex12_27(void)
 {
     ifstream infile("textquery.txt");
     TextQuery tq(infile);
-    tq.print_query("snowman");
+    while(true)
+    {
+        cout << "enter word to look for, or q to quit: ";
+        string s;
+        if(!(cin >> s) || "q" == s)
+        {
+            break;
+        }
+        tq.print_query(s);
+    }
+}
+
+int main(int argc, char const* argv[])
+{
+    ex12_27();
     return 0;
 }
